8dbd75cc6df2d6fa092bfd39a636f3d6a17ed4fe
Pieter Wolfert <pieter@pieterwolfert.com>
2017-06-01 11:31:46 +0200

    Base class to perform random movement.

---
+++ randommovement.py
@@ -0,0 +1,20 @@
+"""
+author: Pieter Wolfert
+"""
+
+class RandomMovement:
+    def __init__(self, ALMotionProxy):
+        #initialization for random movement
+        self.motionproxy = ALMotionProxy
+    def moveRandomLimb(self):
+        #select a limb, move it 
+    def moveRandomAll(self):
+        #move all limbs to a certain degree
+    def moveLeftLeg(self):
+        #move left
+    def moveRightLeg(self):
+        #move right
+    def moveLeftArm(self):
+        #move left arm
+    def moveRightArm(self):
+        #move right arm
