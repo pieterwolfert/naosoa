edce4a247fe775cc10eab65012ed38f244a99fbc
Pieter Wolfert <pieter@pieterwolfert.com>
2017-06-01 10:29:11 +0200

    Module to use for connecting to the robot.

---
+++ connector.py
@@ -0,0 +1,33 @@
+"""
+@author: Pieter Wolfert
+"""
+from naoqi import ALProxy
+
+class RobotConnect:
+    """
+    Class for connecting to robot.
+    IP has to be a string.
+    """
+    def __init__(self, ip):
+        self.robotip = ip
+        self.port = 9559
+
+    """
+    Get proxy with a given name.
+    Returns the specific proxy blob.
+    """
+    def setProxy(self, name):
+        self.nameProxy = ALProxy(name, self.robotip, self.port)
+
+    def setMotionProxy(self):
+        self.motionProxy = ALProxy("ALMotion", self.robotip, self.port)
+
+"""
+Main is purely for testing purposes.
+"""
+def main():
+    naomi = RobotConnect("192.168.0.104")
+    naomi.setMotionProxy()
+
+if __name__ == '__main__':
+    main()
