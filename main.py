25aa5a7fe7bc87afb4ebf321dc3fd7f7ff64eb5e
Weel, W.W. van der (Wouter) <wouter.weel@student.ru.nl>
2017-06-27 10:20:39 +0200

    Implemented movement for all limbs over epochs.

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ from sensorimotorsystem import SensoriMotorSystem
 from randommovement import RandomMovement
 
 import naoqi
+import time
 
 """
 Set parameters
 """
 
-ip = '168.192.1.143'
-port = '9559'
+ip = '192.168.1.143'
+port = 9559
 nr_epochs = 10
-nr_images = 5
+
 
 """
 Create proxies
 """
 
-motionProxy = naoqi.ALProxy("ALMotion", ip , port)
+# Create posture proxy
+postureProxy = naoqi.ALProxy("ALRobotPosture", ip, port)
+# Create motion proxy
+motionProxy = naoqi.ALProxy("ALMotion", ip, port)
 
 """
 Instantiate all needed classes
@@ -32,8 +36,21 @@ vision = VisualSystem()
 sensorimotorsystem = SensoriMotorSystem()
 comparator = Comparator()
 integrator = Integrator()
+randomMovement = RandomMovement(motionProxy)
 
 prev_limb_movements = None
+
+"""
+Preparations
+"""
+postureProxy.goToPosture("LyingBack", 0.7)
+
+#Set joints to standard position
+joints = ["LShoulderPitch", "RShoulderPitch", "RElbowRoll", "LElbowRoll", "LHipPitch", "RHipPitch", "LKneePitch", "RKneePitch"]
+target_angle = [-0.1, -0.1, 0.0, 0.0, -0.1, -0.1, 0.0, 0.0]
+maxSpeedFraction = 0.4
+motionProxy.setAngles(joints, target_angle, maxSpeedFraction)
+time.sleep(2)
 
 """
 Training loop in which the networks are trained on-line
@@ -42,25 +59,35 @@ epoch = 0
 error = 0
 mobile_movement = 0
 
-
-
-while True:
-    # outputs limb movements
-    limb_movements = integrator.limbMovements(error, mobile_movement, epoch)
+for epoch in range(nr_epochs):
+    print("Epoch "+ str(epoch))
+    # outputs limb movement speeds
+    limb_speeds = integrator.limbMovements(error, mobile_movement, epoch)
 
     # use limb_movements to move robot limbs
-    connector.move(limb_movements)
+    connector.move(limb_speeds)
+
+
+    randomMovement.moveRandomAll(epoch)
+    time.sleep(3)
 
     #get a number of images to calculate movement in the last few seconds
-    camera_images = connector.getImages(nr_images) #a stream of images
+    camera_images = connector.getImages() #a stream of images
 
     #calculate mobile movement based on the images
     mobile_movement = vision.getMovement(camera_images)
 
     #receives the limb movements to predict mobile movement
-    predicted_mobile_movement = sensorimotorsystem.getPrediction(mobile_movement, limb_movements)
+    predicted_mobile_movement = sensorimotorsystem.getPrediction(mobile_movement, limb_speeds)
 
     #compare predicted mobile movement with actual mobile movement
-    error = comparator.error(mobile_movement, predicted_mobile_movement, limb_movements)
+    error = comparator.error(mobile_movement, predicted_mobile_movement, limb_speeds)
 
     epoch += 1
+
+
+"""
+End of experiment
+"""
+postureProxy.goToPosture("LyingBack", 0.7)
+motionProxy.rest()
--- randommovement.py
+++ randommovement.py
@@ -3,11 +3,12 @@ author: Pieter Wolfert
 """
 
 import naoqi
+import time
 
 class RandomMovement:
     def __init__(self, ALMotionProxy):
         #initialization for random movement
-        self.motionproxy = ALMotionProxy
+        self.motionProxy = ALMotionProxy
     def moveRandomLimb(self):
         pass#select a limb, move it
 
@@ -16,16 +17,18 @@ class RandomMovement:
 
         #move up or down based on epoch
         if epoch % 2 == 0:
-            target_angle = [-.5]
+            target_angle_legs = [-0.5]
+            target_angle_arms = [0.5]
         else:
-            target_angle = [.5]
+            target_angle_legs = [0.5]
+            target_angle_arms = [-0.5]
 
         #set speeds for different limbs
-        speed = [0.2, 0.4, 0.6, 0.8]
-        self.moveLeftLeg(target_angle, speed[0])
-        self.moveRightLeg(target_angle, speed[1])
-        self.moveLeftArm(target_angle, speed[2])
-        self.moveRightArm(target_angle, speed[3])
+        speed = [0.1, 0.1, 0.1, 0.1]
+        self.moveLeftLeg(target_angle_legs, speed[0])
+        self.moveRightLeg(target_angle_legs, speed[1])
+        self.moveLeftArm(target_angle_arms, speed[2])
+        self.moveRightArm(target_angle_arms, speed[3])
 
     def moveLeftLeg(self, target_angle, maxSpeedFraction):
         #move left leg
@@ -46,10 +49,10 @@ class RandomMovement:
 
 if __name__ == "__main__":
 
-    ip = '192.168.1.138'
-    port = '9559'
+    ip = '192.168.1.143'
+    port = 9559
 
-    nr_epochs = 10
+    nr_epochs = 20
 
     # Create posture proxy
     postureProxy = naoqi.ALProxy("ALRobotPosture", ip, port)
@@ -58,10 +61,19 @@ if __name__ == "__main__":
 
     postureProxy.goToPosture("LyingBack", 0.7)
 
+    #Set joints to standard position
+    joints = ["LShoulderPitch", "RShoulderPitch", "RElbowRoll", "LElbowRoll", "LHipPitch", "RHipPitch", "LKneePitch", "RKneePitch"]
+    target_angle = [-0.1, -0.1, 0.0, 0.0, -0.1, -0.1, 0.0, 0.0]
+    maxSpeedFraction = 0.4
+    motionProxy.setAngles(joints, target_angle, maxSpeedFraction)
+    time.sleep(2)
+
     randomMovement = RandomMovement(motionProxy)
 
     for epoch in range(nr_epochs):
+        print(epoch)
         randomMovement.moveRandomAll(epoch)
+        time.sleep(3)
 
     # Sit
     postureProxy.goToPosture("LyingBack", 0.7)
